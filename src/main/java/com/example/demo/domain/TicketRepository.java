76720ba9fe9e6c90a14ecc321b9ed8377de46693
Vincent Emond <vincent.emond@ticketmaster.com>
2023-10-12 09:14:13 -0400

    Adding ticketDao in memory

--- src/main/java/com/example/demo/domain/TicketRepository.java
+++ src/main/java/com/example/demo/domain/TicketRepository.java
@@ -2,9 +2,11 @@ package com.example.demo.domain;
 
 import com.example.demo.data.TicketDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 
+@Component
 public class TicketRepository {
 
     @Autowired
